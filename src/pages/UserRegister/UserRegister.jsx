0ea42136067d0f652127dfc0d3441a67cbadb8d9
cilasmarques <cilas.marques@ccc.ufcg.edu.br>
2023-03-02 14:59:46 -0300

    Removing user register & adding reviewers selector

--- src/pages/UserRegister/UserRegister.jsx
+++ src/pages/UserRegister/UserRegister.jsx
@@ -10,75 +10,33 @@ import Sidebar from "../../components/Sidebar/Sidebar";
 import { userFindByRole, userRegister, userUpdate } from "../../services/UserService";
 
 // STYLES
-import { Head, Title, FormsConatiner, MediumFormContainer, SmallFormContainer } from "./style.userRegister";
+import { Head, Title, FormsConatiner, SmallFormContainer } from "./style.userRegister";
 import { Grid, GridContent, GridSidebar } from "../../styles/global/Grid";
 import Select from "../../components/Select/Select";
 
 function UserRegister() {
-  const [studentEmail, setStudentEmail] = useState("");
-  const [studentName, setStudentName] = useState("");
-  const [studentEnroll, setStudentEnroll] = useState("");
-
   const [reviewerEmail, setReviewerEmail] = useState("");
   const [reviewerName, setReviewerName] = useState("");
 
   const [updateStudentEmail, setUpdateStudentEmail] = useState("");
   const [updateStudentEnroll, setUpdateStudentEnroll] = useState("");
   
-  const [reviewersOptions, setReviewersOptions] = useState([]);
   const [studentsOptions, setStudentsOptions] = useState([]);
-
   const [isLoading, setIsLoading] = useState(false);
-
-  const loadUser = async (role) => {
-    const response = await userFindByRole(role);
-    if (response?.status === 200) {
-      const users = response.data.users.map((user) => {
-        return { value: user.email, label: user.email }
-      });
-      return users;
-    }
-  };
 
   useEffect(() => {
     const loadData = async () => {
-      const reviewers = await loadUser('REVIEWER');
-      if (reviewers?.length > 0) {
-        setReviewerEmail(reviewers[0].value);
-        setReviewersOptions(reviewers);
-      }
-
-      const students = await loadUser('STUDENT');
-      if (students?.length > 0) {
-        setUpdateStudentEmail(students[0].value);
-        setStudentsOptions(students);
+      const response = await userFindByRole('STUDENT');
+      if (response?.status === 200) {
+        const users = response.data.users.map((user) => {
+          return { value: user.email, label: user.email }
+        });
+        setUpdateStudentEmail(users[0].value);
+        setStudentsOptions(users);
       }
     }
     loadData();
   }, []);
-
-  const handleSubmitStudentRegister = async () => {
-    try {
-      const userData = new FormData();
-      userData.append("email", studentEmail);
-      userData.append("name", studentName);
-      userData.append("enroll", studentEnroll);
-      userData.append("role", "student");
-
-      setIsLoading(true);
-      const response = await userRegister(userData);
-      setIsLoading(false);
-
-      if (response.status === 200) {
-        setStudentEmail("");
-        setStudentName("");
-        setStudentEnroll("");
-        window.alert("Estudante cadastrado com sucesso");
-      }
-    } catch (err) {
-      console.error('Upload failed:', err);
-    }
-  };
 
   const handleSubmitReviewerRegister = async () => {
     try {
@@ -135,17 +93,9 @@ function UserRegister() {
         </Head>
 
         <FormsConatiner>
-          <MediumFormContainer>
-            <p>Cadastro de alunos</p>
-            <Input placeholder="Email do aluno" type="email" onChange={(e) => setStudentEmail(e.target.value)} value={studentEmail} />
-            <Input placeholder="Nome do aluno" type="text" onChange={(e) => setStudentName(e.target.value)} value={studentName} />
-            <Input placeholder="Matricula do aluno" type="numeric" onChange={(e) => setStudentEnroll(e.target.value)} value={studentEnroll} />
-            {isLoading ? <CircularProgress /> : <Button text="Cadastrar aluno" backgroundColor="#497DB1" onClick={handleSubmitStudentRegister} />}
-          </MediumFormContainer>
-
           <SmallFormContainer>
             <p>Cadastro de revisores</p>
-            <Select options={reviewersOptions} onChange={(e) => setReviewerEmail(e.target.value)} />
+            <Input placeholder="Email do revisor" type="email" onChange={(e) => setReviewerEmail(e.target.value)} value={reviewerEmail} />
             <Input placeholder="Nome do revisor" type="text" onChange={(e) => setReviewerName(e.target.value)} value={reviewerName} />
             {isLoading ? <CircularProgress /> : <Button text="Cadastrar revisor" backgroundColor="#497DB1" onClick={handleSubmitReviewerRegister} />}
           </SmallFormContainer>
